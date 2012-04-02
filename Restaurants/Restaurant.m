17181878ccbe68fa0033655368e82a0b03b46b22
Roussina <roussina@gmail.com>
2012-04-02 14:22:01 -0400

    ...dealing with crash.

--- Restaurants/Restaurant.m
+++ Restaurants/Restaurant.m
@@ -16,7 +16,8 @@
         cuisineType = @"French";
         phone = @"212 555 1234";
         review = @"I think Per Se is the best restaurant in the city, because it serves the best, most sophisticated French cuisine you can try today. It is very expensive, but simply devine.";
-        age = @"1986";
+        age = @"26";
+    
     }
     return self;
 }
